88c51df13002d6ce805ebd42fd35d75bc2d425a3
Drew Burns <drewburnsbab@gmail.com>
2015-04-23 23:59:27 +0000

    finished log in and out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
   def show 
     @user = User.find(params[:id]) 
   end
@@ -10,12 +11,14 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
+      log_in(@user)
       redirect_to @user
     else
       render 'new'
     end
   end
   
+    
   private 
   
   def user_params
