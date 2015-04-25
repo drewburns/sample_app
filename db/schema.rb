f97dd9c913d434fa7f47876dc790bc0069e8be25
Drew Burns <drewburnsbab@gmail.com>
2015-04-25 18:18:32 +0000

    user edit, update ,destroy ,index

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150418224959) do
+ActiveRecord::Schema.define(version: 20150425175713) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150418224959) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
